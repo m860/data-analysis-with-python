c32631a32e800c144208581c0387abfb82f98159
jean <jean.ma.1986@gmail.com>
2016-12-02 16:39:58 +0800

    fix

--- practises/002-fmt-data.py
+++ practises/002-fmt-data.py
@@ -1,23 +1,22 @@
 from datetime import datetime
-from pandas import DataFrame,Series
-import pandas as pd;import numpy as np
+from pandas import DataFrame, Series
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
-# 读取文件
-with open('data/SZ#002637.txt','r') as f:
-    # 读取所有的行
-    lines=f.readlines()
+with open('data/SZ#002637.txt', 'r') as f:
+    lines = f.readlines()
     print len(lines)
-    # 删除非数据行
     lines.pop(0)
     lines.pop(0)
     lines.pop(-1)
     print len(lines)
-    # 将数据转换成字典格式
-    fmtdata=[{'date':data[0],'open':float(data[1]),'high':float(data[2]),'low':float(data[3]),'close':float(data[4])} for data in (i.replace('\t',',').replace('\r\n','').split(',') for i in lines)]
+    fmtdata = [{'date': data[0], 'open': float(data[1]), 'high': float(data[2]), 'low': float(data[3]),
+                'close': float(data[4])} for data in
+               (i.replace('\t', ',').replace('\r\n', '').split(',') for i in lines)]
     print fmtdata
-    frame=DataFrame(fmtdata)
+    frame = DataFrame(fmtdata)
     print frame['close'][:10]
-    close_counter=frame['close'].value_counts();
+    close_counter = frame['close'].value_counts();
     print close_counter[10:20]
-    #close_counter[2:6].plot(kind='barh',rot=0)
+    close_counter.plot(kind='barh', rot=0)
