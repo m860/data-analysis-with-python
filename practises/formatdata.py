8517c950d70c7a04fa60694e308d0442e79af4ad
jean <jean.ma.1986@gmail.com>
2016-12-13 09:50:11 +0800

    fix

---
+++ practises/formatdata.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+
+from classes import Stock, format
+import numpy as np
+import os
+
+
+def getFiles(dirname):
+    files = []
+    for dirpath, dirnames, filenames in os.walk(dirname):
+        files.extend([os.path.join(dirpath, f) for f in filenames if f.endswith('.txt')])
+    return files
+
+
+def getName(filepath):
+    basename = os.path.basename(filepath)
+    name, ext = os.path.splitext(basename)
+    return name
+
+
+files = getFiles('original')
+for p in files:
+    format(p, os.path.join('formated', getName(p) + '.json'))
